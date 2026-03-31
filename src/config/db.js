ab04fc969269e95f3716bde73dd3d5e6296cbe44
carlosbonfim <carlosbonfim722@gmail.com>
2026-03-31 20:02:24 -0300

    config: ajustes para o serveless

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-const mongoDbUri = process.env.MONGODB_URI;
 export async function connectDB() {
   try {
-    await mongoose.connect(
-      mongoDbUri
-    );
+   await mongoose.connect(process.env.MONGODB_URI, {
+  serverSelectionTimeoutMS: 5000
+});
     console.log("Banco conectado com sucesso");
   } catch (error) {
     console.error("Erro ao conectar no banco:", error);
