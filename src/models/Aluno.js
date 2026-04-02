c180b52df765333c5bbc77cc869a759754ba0c1c
carlosbonfim <carlosbonfim722@gmail.com>
2026-04-02 18:44:28 -0300

    Update em AlunoSchema e Aluno.js

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
-import { AlunoSchema } from "../schemas/AlunoSchema.js";
+import { AlunoSchema } from "../schemas/AlunoSchema.Js";
 
 export const Aluno = mongoose.model("Aluno", AlunoSchema);
