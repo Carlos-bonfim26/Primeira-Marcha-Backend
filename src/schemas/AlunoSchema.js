4f21f4ead2be66dd21024d77906901c58db955e5
carlosbonfim <carlosbonfim722@gmail.com>
2026-04-05 17:30:50 -0300

    feat: correção em alunoSchema.js

--- src/schemas/AlunoSchema.js
+++ src/schemas/AlunoSchema.js
@@ -1,34 +1,34 @@
 import mongoose from "mongoose";
 
 export const AlunoSchema = new mongoose.Schema({
-    aluno:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"Aluno",
-        required:true
-    },
-    instrutor:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"Instrutor",
-        required:true
-    },
-    dataInicio:{
-        type:Date,
-        required:true
-    },
-    dataFinal:{
-        type:Date,
-        required:true
-    },
-    UF:{
-        type:String,
-        required:true
-    },
-    localAula:{
-        type:String,
-        required:true
-    },
-    statusAula:{
-        type:String,
-        required:true
-    }
+  nome: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  senha: {
+    type: String,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  dataNasc: {
+    type: Date,
+    required: true,
+  },
+  cidade: {
+    type: String,
+    required: true,
+  },
+  UF: {
+    type: String,
+    required: true,
+  },
 });
